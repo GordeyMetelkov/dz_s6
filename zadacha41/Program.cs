fe961ae89ebacabbe031ec99f03f021dab4ab861
Gordey <gordeymetelkov@icloud.com>
2022-06-28 22:37:35 +0300

    init

---
+++ zadacha41/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите количество вводимых чисел:");
+int N = Convert.ToInt32(Console.ReadLine());
+int PosNumbers (int curN)
+{
+    int countPos = 0;
+    for (int i = 0; i<curN; i++)
+    {
+        Console.WriteLine("Введите число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0) countPos++;   
+    }
+    return countPos;
+}
+Console.WriteLine("Количество положительных чисел равно "+ PosNumbers(N));
---
+++ zadacha43/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine($"Первая прямая задана уравнением 'y = k1 * x + b1'");
+Console.WriteLine($"Вторая прямая задана уравнением 'y = k2 * x + b2'");
+Console.WriteLine("Введите значение k1:");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b1:");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2:");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2:");
+int b2 = Convert.ToInt32(Console.ReadLine());
+tochki (k1, b1, k2, b2);
+void tochki (int curk1, int curb1, int curk2, int curb2)
+{
+    if (curk1 == curk2) Console.WriteLine("Введите разные значения curk1 и curk2:");
+    else 
+    {
+        double curx = (curb2-curb1) / (curk1-curk2);
+        double cury = curk1*curx + curb1;
+        Console.WriteLine($"Точка пересечения двух прямых имеет координаты [{curx}; {cury}]");
+    }
+}
